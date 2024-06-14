1ea6f01d24d8c2af82d98bfce5eba360fbdeb80a
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-14 12:11:43 +0800

    update

--- src/screens/dashboard/HomeScreen.tsx
+++ src/screens/dashboard/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import moment from "moment";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import { StackNavigationProp } from "@react-navigation/stack";
 import type { TStackParamsList } from "@/types/navigation";
 
@@ -33,7 +34,11 @@ export const HomeScreen: React.FC<TScreenProps> = (props) => {
   return (
     <View className="flex-1  bg-white">
       <View className="w-full h-[200px] bg-yellow-300 p-5">
-        <View className="w-full flex flex-col justify-between">
+        <View className="w-full flex flex-col justify-between relative">
+          <View className="absolute top-2 right-2">
+            <AntDesign name="bells" size={24} color="black" />
+          </View>
+
           <View className="flex flex-row items-center gap-x-3">
             <View className="w-[48px] h-[48px] bg-slate-800 rounded-full" />
             <View>
@@ -44,9 +49,26 @@ export const HomeScreen: React.FC<TScreenProps> = (props) => {
 
           <Text className="text-xs text-gray-600 font-medium mt-2">{datetime}</Text>
         </View>
+
+        <Text className="text-xs text-gray-700 font-medium mt-5">Your Current Booking &mdash;</Text>
+        <Text className="text-sm text-red-700 font-bold mt-2">N/A</Text>
       </View>
 
-      <View className="w-full flex-1 px-2 py-5"></View>
+      <View className="w-full flex-1 px-2 py-5">
+        <View className="flex-1">
+          <View className="bg-white pb-4" style={{ zIndex: 999 }}>
+            <TextInput className="w-full h-[50px] text-xs border border-gray-700 rounded px-2" placeholder="Search Destination" />
+            <Text className="text-xs font-bold pt-4">Available Bus Routes:</Text>
+          </View>
+          <ScrollView showsVerticalScrollIndicator={false} className="flex flex-col gap-y-4 pb-[80px]">
+            <View className="w-full h-[130px] bg-slate-50 border border-gray-300 rounded-lg"></View>
+            <View className="w-full h-[130px] bg-slate-50 border border-gray-300 rounded-lg"></View>
+            <View className="w-full h-[130px] bg-slate-50 border border-gray-300 rounded-lg"></View>
+            <View className="w-full h-[130px] bg-slate-50 border border-gray-300 rounded-lg"></View>
+            <View className="w-full h-[130px] bg-slate-50 border border-gray-300 rounded-lg"></View>
+          </ScrollView>
+        </View>
+      </View>
     </View>
   );
 };
