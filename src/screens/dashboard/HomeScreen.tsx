8f09f283316efa035f5dd4144757a5f50beb40d6
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-14 11:56:39 +0800

    update

--- src/screens/dashboard/HomeScreen.tsx
+++ src/screens/dashboard/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import moment from "moment";
-import { View, Text, Pressable } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, Text } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import type { TStackParamsList } from "@/types/navigation";
 
@@ -9,9 +10,17 @@ type TScreenProps = {
 };
 
 export const HomeScreen: React.FC<TScreenProps> = (props) => {
+  const [user, setUser] = React.useState<any>(null);
   const [datetime, setDatetime] = React.useState("");
 
+  const getUserData = async () => {
+    const data = await AsyncStorage.getItem("user");
+    setUser(JSON.parse(data!));
+  };
+
   React.useEffect(() => {
+    getUserData();
+
     const interval = setInterval(() => {
       setDatetime(moment().format("MMMM D, Y hh:mm:ss A"));
     }, 1000);
@@ -24,13 +33,20 @@ export const HomeScreen: React.FC<TScreenProps> = (props) => {
   return (
     <View className="flex-1  bg-white">
       <View className="w-full h-[200px] bg-yellow-300 p-5">
-        <Text className="text-lg font-bold">Welcome User!</Text>
-        <Text className="text-xs text-gray-500 font-medium">Ready to travel?</Text>
+        <View className="w-full flex flex-col justify-between">
+          <View className="flex flex-row items-center gap-x-3">
+            <View className="w-[48px] h-[48px] bg-slate-800 rounded-full" />
+            <View>
+              <Text className="text-md font-bold">{user ? `${user.firstName} ${user.lastName}` : null}</Text>
+              <Text className="text-xs text-gray-500 font-medium">Ready to travel?</Text>
+            </View>
+          </View>
+
+          <Text className="text-xs text-gray-600 font-medium mt-2">{datetime}</Text>
+        </View>
       </View>
 
-      <View className="w-full flex-1 px-2 py-5">
-        <Text className="text-gray-800 font-bold">{datetime}</Text>
-      </View>
+      <View className="w-full flex-1 px-2 py-5"></View>
     </View>
   );
 };
