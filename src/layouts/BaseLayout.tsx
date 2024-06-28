624456d023a0e2f6b834e2cfcedbf877a6e7d28b
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-06-28 11:36:32 +0800

    wip home contents

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,49 +1,82 @@
 import React from "react";
-import { useNavigation, NavigationProp } from "@react-navigation/native";
+import { useNavigation, useRoute, NavigationProp } from "@react-navigation/native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Pressable } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { PageHeader } from "@/components/shared";
 import { useAuth } from "@/hooks";
 import { Toast } from "react-native-toast-alert";
 
 const BottomNav: React.FC<{ navigate: NavigationProp<any>["navigate"] }> = (props) => {
-  const iconStyle = { size: 24, color: "#666" };
+  const route = useRoute();
 
   const goToScreen = (screenName: string) => {
     // @ts-ignore
   };
+
+  const NavButton: React.FC<{
+    screenName: string;
+    icon: string;
+  }> = (props) => {
+    const getButtonColor = () => {
+      const isActive = route.name === props.screenName;
+
+      if (isActive) {
+        return {
+          iconColor: "lightcoral",
+          labelColor: "text-red-400",
+        };
+      }
+
+      return {
+        iconColor: "#777",
+        labelColor: "text-gray-500",
+      };
+    };
+
+    const { iconColor, labelColor } = getButtonColor();
+
+    return (
+      <Pressable className="flex flex-col items-center gap-y-2">
+        <Ionicons name={props.icon as any} size={24} color={iconColor} />
+        <Text className={`text-xs text-center uppercase font-medium ${labelColor}`}>
+          Home
+        </Text>
+      </Pressable>
+    );
+  };
+
+  const buttons = [
+    {
+      name: "HOME_SCREEN",
+      label: "Home",
+      icon: "home-outline",
+    },
+    {
+      name: "TICKETS_SCREEN",
+      label: "Tickets",
+      icon: "ticket-outline",
+    },
+    {
+      name: "PAYMENT_HISTORY_SCREEN",
+      label: "Payments",
+      icon: "card-outline",
+    },
+    {
+      name: "MY_ACCOUNT_SCREEN",
+      label: "Account",
+      icon: "person-outline",
+    },
+  ];
 
   return (
     <View
       className="w-full h-[70px] flex flex-row justify-between items-center fixed bottom-0 border-gray-300 px-5"
       style={{ borderTopWidth: 1 }}
     >
-      <Pressable className="flex flex-col items-center gap-y-2">
-        <AntDesign name="home" size={iconStyle.size} color={iconStyle.color} />
-        <Text className="text-xs text-center uppercase text-gray-600 font-bold">
-          Home
-        </Text>
-      </Pressable>
-      <Pressable className="flex flex-col items-center gap-y-2">
-        <Ionicons name="ticket-outline" size={iconStyle.size} color={iconStyle.color} />
-        <Text className="text-xs text-center uppercase text-gray-600 font-bold">
-          Tickets
-        </Text>
-      </Pressable>
-      <Pressable className="flex flex-col items-center gap-y-2">
-        <AntDesign name="creditcard" size={iconStyle.size} color={iconStyle.color} />
-        <Text className="text-xs text-center uppercase text-gray-600 font-bold">
-          Payments
-        </Text>
-      </Pressable>
-      <Pressable className="flex flex-col items-center gap-y-2">
-        <AntDesign name="user" size={iconStyle.size} color={iconStyle.color} />
-        <Text className="text-xs text-center uppercase text-gray-600 font-bold">
-          Account
-        </Text>
-      </Pressable>
+      {buttons.map((button) => (
+        <NavButton icon={button.icon} screenName={button.name} />
+      ))}
     </View>
   );
 };
@@ -60,8 +93,6 @@ export const BaseLayout: React.FC<{
   const { navigate } = useNavigation();
 
   React.useEffect(() => {
-    console.log("baselayout-isAuthenticated", isAuthenticated);
-
     if (props.authGuard) {
       if (!isAuthenticated) {
         Toast.error("You must login");
--- src/screens/dashboard/HomeScreen.tsx
+++ src/screens/dashboard/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, RefreshControl } from "react-native";
 import { BaseLayout } from "@/layouts";
 import { HeaderNav } from "@/components/domains";
 import type { StackParamsList } from "@/types/navigation";
@@ -11,22 +11,35 @@ type Props = {
 };
 
 export const HomeScreen: React.FC<Props> = (props) => {
+  const [refetchLoading, setRefetchLoading] = React.useState<boolean>(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefetchLoading(true);
+
+    setTimeout(() => {
+      setRefetchLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <BaseLayout hasFooter>
       <HeaderNav />
 
-      <View className="px-3 mt-2">
-        <View>
-          <Text className="text-xs text-gray-700 font-medium">
-            Current Active Booking
-          </Text>
-          <EmptyData />
-        </View>
-
+      <View className="flex-1 px-3 mt-2">
         <ScrollView
-          className="flex-col gap-4 mt-2"
+          className="flex-col gap-y-4 pt-3"
+          refreshControl={
+            <RefreshControl refreshing={refetchLoading} onRefresh={onRefresh} />
+          }
           showsHorizontalScrollIndicator={false}
         >
+          <View>
+            <Text className="text-xs text-gray-700 font-medium">
+              Current Active Booking
+            </Text>
+            <EmptyData />
+          </View>
+
           <View>
             <Text className="text-xs text-gray-700 font-medium">
               Promos & Announcements
